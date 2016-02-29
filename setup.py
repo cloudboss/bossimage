c83abe203dc5fa1e014e86b6b8da0dfa750ceded
Joseph Wright <rjosephwright@gmail.com>
2016-02-29 08:05:15 -0800

    Create project files

---
+++ setup.py
@@ -0,0 +1,25 @@
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
+
+config = {
+    'description': 'Tool to create AMIs with Ansible',
+    'author': 'Joseph Wright',
+    'url': 'https://github.com/cloudboss/bossimage',
+    'download_url': 'https://github.com/cloudboss/bossimage/releases',
+    'author_email': 'rjosephwright@gmail.com',
+    'version': '0.1',
+    'install_requires': [
+        'ansible',
+        'boto3',
+        'click'
+    ],
+    'packages': ['bossimage'],
+    'entry_points': {
+        'console_scripts': ['bi = bossimage.cli:main']
+    },
+    'name': 'bossimage'
+}
+
+setup(**config)
